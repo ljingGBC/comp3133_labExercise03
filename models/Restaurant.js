cd303fc8dd9e3b1949f2317b8321950e4b2a38f1
ljingGBC <lei.jing@georgebrown.ca>
2021-02-06 18:55:19 -0500

    update Restaurant.js

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -41,6 +41,21 @@ const RestaurantSchema = new mongoose.Schema({
 });
 
 
+RestaurantSchema.post('init', (doc) => {
+    console.log('%s has been initialized from the db', doc._id);
+  });
+  
+RestaurantSchema.post('validate', (doc) => {
+    console.log('%s has been validated (but not saved yet)', doc._id);
+});
+  
+RestaurantSchema.post('save', (doc) => {
+    console.log('%s has been saved', doc._id);
+});
+  
+RestaurantSchema.post('remove', (doc) => {
+    console.log('%s has been removed', doc._id);
+});  
 
 const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
 module.exports = Restaurant;
